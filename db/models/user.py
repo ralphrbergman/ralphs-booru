15c9b508575afd940ea04b91bb0f62a0a8d93b6a
ralphrbergman <ralfs@esmuralfs.lv>
2025-10-10 21:53:31 +0300

    Users should have a registered time

--- db/models/user.py
+++ db/models/user.py
@@ -1,14 +1,17 @@
+from datetime import datetime
 from typing import Optional
 
 from flask import url_for
 from flask_login import UserMixin
-from sqlalchemy import String
+from sqlalchemy import String, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
 
 from db import db
 
 class User(db.Model, UserMixin):
     id: Mapped[int] = mapped_column(primary_key = True)
+    created: Mapped[datetime] = mapped_column(default = func.now())
+
     name: Mapped[str] = mapped_column(String(length = 20), nullable = False, unique = True)
     mail: Mapped[str] = mapped_column(nullable = True, unique = True)
     password: Mapped[str] = mapped_column(String(length = 128), nullable = False)
