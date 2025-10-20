e9ebadfdc2b511272a40241d3a0407dd068a982e
ralphrbergman <ralfs@esmuralfs.lv>
2025-10-20 15:35:36 +0300

    Prevent duplicate records for tag

--- db/models/tag_assoc.py
+++ db/models/tag_assoc.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, Table
+from sqlalchemy import Column, ForeignKey, Integer, Table, UniqueConstraint
 
 from db import db
 
@@ -6,5 +6,6 @@ TagAssociation = Table(
     'tag_association',
     db.Model.metadata,
     Column('post_id', Integer, ForeignKey('post.id')),
-    Column('tag_id', Integer, ForeignKey('tag.id'))
+    Column('tag_id', Integer, ForeignKey('tag.id')),
+    UniqueConstraint('post_id', 'tag_id', name = 'uq_post_tag')
 )
