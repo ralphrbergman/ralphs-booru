c1a6b694fda4857a29407bb0e42338d90c8dc151
ralphrbergman <ralfs@esmuralfs.lv>
2025-12-22 16:35:40 +0200

    Comments page

--- api/comment.py
+++ api/comment.py
@@ -1,4 +1,33 @@
+from typing import Literal, Optional
+
+from flask_sqlalchemy.pagination import SelectPagination
+from sqlalchemy import select
+
 from db import Comment, Post, User, db
+
+DEFAULT_LIMIT = 20
+DEFAULT_SORT = 'desc'
+LIMIT_THRESHOLD = 100
+
+def browse_comment(
+    limit: Optional[int] = DEFAULT_LIMIT,
+    page: Optional[int] = 1,
+    sort_str: Optional[Literal['asc', 'desc']] = DEFAULT_SORT
+) -> SelectPagination:
+    if limit and limit > LIMIT_THRESHOLD:
+        limit = LIMIT_THRESHOLD
+
+    stmt = select(Comment).order_by(
+        getattr(Comment.id, sort_str)()
+    )
+
+    comments = db.paginate(
+        stmt,
+        page = page,
+        per_page = limit
+    )
+
+    return comments
 
 def create_comment(content: str, author: User, post: Post) -> Comment:
     """
---
+++ blueprint/comment.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, render_template
+
+from api import browse_comment, count_all_posts
+from .utils import create_pagination_bar
+
+comment_bp = Blueprint(
+    name = 'Comment',
+    import_name = __name__
+)
+
+@comment_bp.route('/comments')
+def comment_page():
+    page = request.args.get('page', default = 1, type = int)
+
+    comments = browse_comment(page = page)
+    bar = create_pagination_bar(page, comments.pages, 'Comment.comment_page')
+
+    return render_template(
+        'comment.html',
+        bar = bar,
+        comments = comments,
+        current_page = page,
+        post_count = count_all_posts()
+    )
