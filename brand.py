46b731b637f26196ef22698ae5f674d59295264d
ralphrbergman <ralfs@esmuralfs.lv>
2026-01-02 14:30:10 +0200

    Include page description & author in brand config

--- brand.py
+++ brand.py
@@ -1,11 +1,16 @@
 brand = {
     'contact': {
-        'address': 'example.com',
-        'mail': 'user@example.com',
-        'media': {},
-        'name': 'Your Name',
+        'address': 'www.esmuralfs.lv',
+        'mail': 'ralfs@esmuralfs.lv',
+        'media': {
+            'Discord': 'ralphwuu',
+            'GitHub': 'https://github.com/ralphrbergman',
+            'Instagram': 'https://instagram.com/ralphrbergman/'
+        },
+        'name': 'Ralph Rainer Bergman',
     },
-    'full_brand': 'Ralphs Booru',
+    'description': 'Ralph\'s kind of Booru.',
+    'full_brand': 'EsmuRalfs.lv',
     'copyright': '(c) 2025 - present',
     'version': '0.1.0',
     'source-url': 'https://github.com/ralphrbergman/ralphs-booru'
