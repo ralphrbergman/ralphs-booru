92848b0c12d4b387da2616e1163c060bf08feca8
ralphrbergman <ralfs@esmuralfs.lv>
2026-01-13 14:32:00 +0200

    Added 404 handler for REST API

--- blueprint/api/api.py
+++ blueprint/api/api.py
@@ -10,6 +10,10 @@ api_bp = Blueprint(
     url_prefix = '/api'
 )
 
+@api_bp.errorhandler(404)
+def _404_handler(exc):
+    return {'message': 'You are at the wrong place', 'code': 404}, 404
+
 api_bp.register_blueprint(comment_bp)
 api_bp.register_blueprint(post_bp)
 api_bp.register_blueprint(posts_bp)
