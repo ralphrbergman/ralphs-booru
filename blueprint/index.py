807baa84ca2d606ccf213fa7691b889234daa7e3
ralphrbergman <ralfs@esmuralfs.lv>
2026-01-15 15:04:30 +0200

    Removed hanging import

--- blueprint/index.py
+++ blueprint/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, url_for
 
 from api import count_all_posts
 from form import SearchForm
