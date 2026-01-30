1adccf244fc6f70323c37ec401671e450dab45c3
ralphrbergman <ralfs@esmuralfs.lv>
2026-01-30 16:58:25 +0200

    Improved tagging experience & added advanced tagging for upload page

--- static/js/tag_handler.js
+++ static/js/tag_handler.js
@@ -1,6 +1,28 @@
 {
     const InteractiveTags = document.getElementById('interactive-tags');
 
+    function getSpans() {
+        return document.querySelectorAll('span.tag');
+    }
+
+    function getSpanByContent(element) {
+        const SpanTags = getSpans();
+
+        for (let i = 0; i < SpanTags.length; i++) {
+            let span = SpanTags[i];
+
+            if (element.tagName === 'SPAN') {
+                if (span.textContent == element.textContent && span !== element) {
+                    return span;
+                }
+            } else {
+                if (span.textContent == element.value && span !== element) {
+                    return span;
+                }
+            }
+        }
+    }
+
     function manageSpan(span) {
         function handleSpan() {
             if (span.textContent.trim().length === 0) {
@@ -8,17 +30,28 @@
             }
         }
 
-        span.addEventListener('keyup', function(event) {
-            handleSpan();
+        span.addEventListener('keydown', function(event) {
+            if (event.key === 'Enter' || event.key === ' ') {
+                event.preventDefault();
+
+                if (!getSpanByContent(span)) {
+                    span.nextSibling.focus();
+                }
+            }
         });
 
         span.addEventListener('focusout', function(event) {
+            if (getSpanByContent(span)) {
+                span.remove();
+                return;
+            }
+
             handleSpan();
         });
     }
 
     // Register event listeners for already existing tag blocks.
-    const SpanTags = document.querySelectorAll('span.tag');
+    const SpanTags = getSpans();
 
     SpanTags.forEach(function(span) {
         manageSpan(span);
@@ -33,7 +66,7 @@
 
             const Value = TagInput.value.trim();
 
-            if (Value) {
+            if (Value && !getSpanByContent(TagInput)) {
                 const span = document.createElement('span');
 
                 span.className = 'tag';
